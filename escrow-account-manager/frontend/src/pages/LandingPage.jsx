26febeeb38e6b8829b5441b941b2ee722f4bf14b
nabonyimanajospin <jospinnabonyimana@gmail.com>
2026-07-26 23:45:37 +0200

    fix: add missing Link import in LandingPage

--- escrow-account-manager/frontend/src/pages/LandingPage.jsx
+++ escrow-account-manager/frontend/src/pages/LandingPage.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { resolveImageUrl } from '../utils/imageUtils';
 import axios from '../api/axiosConfig';
 import StatusBadge from '../components/StatusBadge';
